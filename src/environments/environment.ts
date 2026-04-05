9a36ed9899a84bef21c874bc045c97bd96c0f38b
varshini-valt <sathyasaivarshini@gmail.com>
2026-04-05 16:44:26 +0530

    DSG

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  demoMode: true,
+  sfAPI: 'https://your-api-domain.com/api/v1/sf',
+  getProgressiveProfileQuestionsApi: 'https://your-api-domain.com/api/v1/getQuestions',
+  isUserExistAPI: 'https://your-api-domain.com/api/v1/student',
+  studentExistInCognito: 'https://your-api-domain.com/api/v1/cognitoCheck',
+  postPPRegistration: 'https://your-api-domain.com/api/v1/student',
+  userPoolId: 'ap-southeast-1_XXXXXXXXX',
+  userPoolWebClientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+  endpoint: 'https://your-cognito-endpoint.com',
+  oneTrustDomainScript: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+  gtmId: 'GTM-XXXXXXX'
+}
